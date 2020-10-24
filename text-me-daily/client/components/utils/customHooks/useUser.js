a757ee0e53b5ecadb4d59b1cc51fe916ab891464
Doug McKay <douglasamckay@gmail.com>
2020-10-24 11:21:08 -0600

    updated user hook

--- text-me-daily/client/components/utils/customHooks/useUser.js
+++ text-me-daily/client/components/utils/customHooks/useUser.js
@@ -5,13 +5,13 @@ import useLocalStorage from "./useLocalStorage"
 
 export default function useUser() {
   const [locstor, setLocstor] = useLocalStorage('tmd_user');
-  const ref = useRef();
+  const ref = useRef(null);
 
   const fetcher = url => Axios.get(url, { headers: { 'authorization': locstor.token } }).then(res => res.data);
   const { data, error } = useSWR(`${document.location.protocol}//${document.location.hostname}:${locstor.apiPort}/api/user`, fetcher);
 
   if (error) {
-    ref.current = 'nothing'
+    ref.current = null
     return ref.current
   }
 
